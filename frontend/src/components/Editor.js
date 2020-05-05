5a8c33a360d49898ebc744e503fffbd762aa2c54
ahmed-28 <ahmedshamsudeen2000@gmail.com>
2020-05-05 17:08:34 +0530

    Feat:homepage and editor

---
+++ frontend/src/components/Editor.js
@@ -0,0 +1,25 @@
+import React,{useState} from 'react';
+import AceEditor from 'react-ace';
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-monokai";
+
+export function Editor(){
+    const [code,setCode] = useState("");
+    const handleChange = (value) => {
+        setCode(value);
+        console.log(value);
+    }
+    return(
+        <div>
+        <AceEditor
+            mode="javascript"
+            theme="monokai"
+            style={{width:'100%'}}
+            value={code}
+            onChange={handleChange}
+            name="UNIQUE_ID_OF_DIV"
+            editorProps={{ $blockScrolling: true }}
+        />
+        </div>
+    );
+}
